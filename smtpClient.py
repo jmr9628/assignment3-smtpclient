02bdc4a6d03e07ac675f5da597f8b26b558ba50e
jmr9628 <jmr9628@nyu.edu>
2025-02-24 17:21:12 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -5,11 +5,15 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     msg = "\r\n My message"
     endmsg = "\r\n.\r\n"
 
-    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
+    # Choose a mail server (e.g. Google mail server) if you want to verify the 
+    # script beyond GradeScope
 
-    # Create socket called clientSocket and establish a TCP connection with mailserver and port
+    # Create socket called clientSocket and establish a TCP connection with 
+    # mailserver and port
 
     # Fill in start
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, port))
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
@@ -27,26 +31,55 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
+    mailFromCommand = 'MAIL FROM: john@john.com\r\n'
+    clientSocket.send(mailFromCommand.encode())
+    recv2 = clientSocket.recv(1024).decode()
+    #print(recv2)
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
+    rcptToCommand = 'RCPT To: raya@raya.com\r\n'
+    clientSocket.send(rcptToCommand.encode())
+    recv3 = clientSocket.recv(1024).decode()
+    #print(recv3)
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
+    dataCommand = 'DATA\r\n'
+    clientSocket.send(dataCommand.encode())
+    recv4 = clientSocket.recv(1024).decode()
+    #print(recv4)
     # Fill in end
 
     # Send message data.
     # Fill in start
+    messageData = 'Date: Mon, 13 Jan 2025 00:00:00 -0500\r\n' \
+                    'From: john <john@john.com>\r\n' \
+                    'Subject: Happy Anniversary\r\n' \
+                    'To: raya@raya.com\r\n' \
+                    '\r\n' \
+                    'I love you! How about we grab some sushi tonight!\r\n' \
+                    'Love,\r\n' \
+                    'John\r\n'
+    clientSocket.send(messageData.encode())
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
+    messageEnd = '.\r\n'
+    clientSocket.send(messageEnd.encode())
+    recv5 = clientSocket.recv(1024).decode()
+    #print(recv5)
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
+    smtpQuit = 'QUIT\r\n'
+    clientSocket.send(smtpQuit.encode())
+    recv6 = clientSocket.recv(1024).decode()
+    #print(recv6)
     # Fill in end
 
 
